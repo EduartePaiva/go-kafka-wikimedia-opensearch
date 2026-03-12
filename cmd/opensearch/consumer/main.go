b09097b2af15e619e73d61a95d3ddb6f9dcb8361
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2026-03-12 12:10:47 -0300

    creating open search index

--- cmd/opensearch/consumer/main.go
+++ cmd/opensearch/consumer/main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"log"
+
+	"github.com/eduartepaiva/go-kafka-wikimedia-opensearch/internal/opensearch"
+)
+
+func main() {
+	// create open search client
+	oshClient, err := opensearch.NewOpenSearchClient([]string{"http://localhost:9200"})
+	if err != nil {
+		log.Fatal("error while creating opensearch client: ", err)
+	}
+
+	// create index
+	err = oshClient.CreateIndex("wikimedia")
+	if err != nil {
+		log.Fatal("error creating wikimedia index: ", err)
+	}
+
+	// create kafka consumer client
+
+	// main code logic
+
+	// close things
+
+}
--- internal/opensearch/opensearch.go
+++ internal/opensearch/opensearch.go
@@ -1,22 +1,62 @@
 package opensearch
 
 import (
+	"context"
 	"crypto/tls"
+	"fmt"
 	"net/http"
+	"strings"
 
 	osh "github.com/opensearch-project/opensearch-go/v4"
+	"github.com/opensearch-project/opensearch-go/v4/opensearchapi"
 )
 
-type OpenSearchClient struct {
-	Client *osh.Client
+type Client struct {
+	api *opensearchapi.Client
 }
 
-func NewOpenSearchClient() (OpenSearchClient, error) {
-	client, err := osh.NewClient(osh.Config{
-		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+func NewOpenSearchClient(addresses []string) (*Client, error) {
+	client, err := opensearchapi.NewClient(
+		opensearchapi.Config{
+			Client: osh.Config{
+				Transport: &http.Transport{
+					TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+				},
+				Addresses: addresses,
+			},
 		},
-		Addresses: []string{"http://localhost:9200"},
-	})
-	return OpenSearchClient{Client: client}, err
+	)
+	return &Client{api: client}, err
+}
+
+func (c *Client) CreateIndex(indexName string) error {
+	mapping := strings.NewReader(`{
+    "settings": {
+        "index": {
+            "number_of_shards": 1,
+            "number_of_replicas": 0
+            }
+        }
+    }`)
+	ctx := context.Background()
+
+	res, _ := c.api.Indices.Exists(ctx, opensearchapi.IndicesExistsReq{Indices: []string{indexName}})
+	if res.StatusCode == http.StatusOK {
+		fmt.Printf("Index \"%s\" already exists.\n", indexName)
+		return nil
+	}
+
+	createIndexResponse, err := c.api.Indices.Create(
+		ctx,
+		opensearchapi.IndicesCreateReq{
+			Index: indexName,
+			Body:  mapping,
+		},
+	)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Created Index: %s\nShards Acknowledged: %t\n", createIndexResponse.Index, createIndexResponse.ShardsAcknowledged)
+
+	return nil
 }
